3582ed6f4df004fec22e50b58276e4c0d09c35d9
Sohan <sohaninfo@gmail.com>
2016-07-01 14:55:24 +0530

    New changes

--- src/ch01/ch01.js
+++ src/ch01/ch01.js
@@ -2,7 +2,7 @@
  * Created by Sohan on 3/25/2016.
  */
 
-function appendP() {
+ function appendP() {
     var p = d3.select("body").append("p");
     p.text("This is the paragraph appended by d3!");
 }
@@ -10,42 +10,42 @@ function appendP() {
 function appendP2() {
     var p = d3.select("body").append("p");
     p.text("This is the second paragraph appended by d3! It is in color.")
-        .style("color", "blue");
+    .style("color", "blue");
 }
 
 function appendP3() {
     var dataset = [1, 2, 3, 4, 5];
 
     var p = d3.select("body").selectAll("p")
-        .data(dataset)
-        .enter()
-        .append("p")
-        .text("New pragraph added through dataset.");
+    .data(dataset)
+    .enter()
+    .append("p")
+    .text("New pragraph added through dataset.");
 }
 
 function appendP4() {
     var dataset = [1, 2, 3, 4, 5];
 
     var p = d3.select("body").selectAll("p")
-        .data(dataset)
-        .enter()
-        .append("p")
-        .text("New pragraph:")
-        .text(function(d){return "This is number: " + d;});
+    .data(dataset)
+    .enter()
+    .append("p")
+    .text("New pragraph:")
+    .text(function(d){return "This is number: " + d;});
 }
 
 function appendP5() {
     var dataset = [1, 2, 3, 4, 5];
 
     var p = d3.select("body").selectAll("p")
-        .data(dataset)
-        .enter()
-        .append("p")
-        .text("New pragraph")
-        .style("color", function(d) {
-            if (d % 2 == 0)
-                return "blue";
-            else
-                return "red";
-        });
+    .data(dataset)
+    .enter()
+    .append("p")
+    .text("New pragraph")
+    .style("color", function(d) {
+        if (d % 2 == 0)
+            return "blue";
+        else
+            return "red";
+    });
 }
